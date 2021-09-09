c16b711cd7edd203612c1252dedd59f46d1729e5
xuhuanli <xuhuanli2017@gmail.com>
2021-09-09 10:31:27 +0800

    .

--- app/src/main/java/com/gome/customview/MainActivity.kt
+++ app/src/main/java/com/gome/customview/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.gome.customview
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.DecelerateInterpolator
+import com.gome.customview.myviews.QQStepView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val qqStepView = findViewById<QQStepView>(R.id.qq_sv)
+        qqStepView.setStepMax(4000)
+        ObjectAnimator.ofInt(0, 3000).apply {
+            duration = 1000
+            interpolator = DecelerateInterpolator()
+            addUpdateListener {
+                val value = it.animatedValue as Int
+                qqStepView.setCurrentStep(value)
+            }
+            start()
+        }
     }
 }
--- app/src/main/java/com/gome/customview/myviews/QQStepView.kt
+++ app/src/main/java/com/gome/customview/myviews/QQStepView.kt
@@ -1,10 +1,7 @@
 package com.gome.customview.myviews
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import com.gome.customview.R
@@ -18,13 +15,18 @@ import kotlin.math.min
 class QQStepView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+    private var mCurrentStep: Int = 0
+    private var mStepMax: Int = 0
     private var mOuterColor: Int
     private var mInnerColor: Int
     private var mBorderWidth: Int
     private var mStepTextSize: Int
     private var mStepTextColor: Int
     private var mOutPaint: Paint
+    private var mInnerPaint: Paint
+    private var mTextPaint: Paint
     private val mRectF = RectF()
+    private val mTextBounds = Rect()
 
     init {
         val ta = context.obtainStyledAttributes(attrs, R.styleable.QQStepView)
@@ -42,6 +44,19 @@ class QQStepView @JvmOverloads constructor(
             strokeWidth = mBorderWidth.toFloat()
             color = mOuterColor
             style = Paint.Style.STROKE
+            strokeCap = Paint.Cap.ROUND
+        }
+        mInnerPaint = Paint().apply {
+            isAntiAlias = true
+            strokeWidth = mBorderWidth.toFloat()
+            color = mInnerColor
+            style = Paint.Style.STROKE
+            strokeCap = Paint.Cap.ROUND
+        }
+        mTextPaint = Paint().apply {
+            isAntiAlias = true
+            color = mStepTextColor
+            textSize = mStepTextSize.toFloat()
         }
     }
 
@@ -68,9 +83,25 @@ class QQStepView @JvmOverloads constructor(
         mRectF.set((center - r).toFloat(), (center - r).toFloat(), (center + r).toFloat(),
             (center + r).toFloat()
         )
+        if (mStepMax == 0) return
         canvas.drawArc(mRectF, 135f, 270f, false, mOutPaint)
         // 2. 画内圆弧
-
+        canvas.drawArc(mRectF, 135f, (mCurrentStep/mStepMax.toFloat()) * 270f, false, mInnerPaint)
         // 3. 画文字
+        val stepText = mCurrentStep.toString()
+        mTextPaint.getTextBounds(stepText, 0, stepText.length, mTextBounds)
+        val xOffset = width/2 - mTextBounds.width()/2
+        val fontMetrics = mTextPaint.fontMetrics
+        val baseLine = height/2 + (fontMetrics.bottom - fontMetrics.top)/2
+        canvas.drawText(stepText, xOffset.toFloat(), baseLine.toFloat(), mTextPaint)
+    }
+
+    fun setStepMax(stepMax: Int) {
+        this.mStepMax = stepMax
+    }
+
+    fun setCurrentStep(currentStep: Int) {
+        this.mCurrentStep = currentStep
+        invalidate()
     }
 }
